ad8ec5fd0ec0387618a7746e1f61ef6b865fb8bc
Amir Bhujel <bhujel.amir@gmail.com>
2015-12-02 23:19:34 -0600

    Updated the clickListener

--- app/src/main/java/com/almightyamir/assignment4/WorkingActivity.java
+++ app/src/main/java/com/almightyamir/assignment4/WorkingActivity.java
@@ -13,6 +13,8 @@ import android.widget.TextView;
 
 public class WorkingActivity extends Activity implements View.OnClickListener {
 
+    TextView result;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,7 +29,7 @@ public class WorkingActivity extends Activity implements View.OnClickListener {
         layoutMain.addView(layoutTitle);
 
         TextView heading = new TextView(this);
-        heading.setText("DETECTING INPUT EVENTS");
+        heading.setText("DETECTING INPUT EVENTS \n (Click Or Long Press)");
         heading.setHeight(100);
         heading.setGravity(Gravity.TOP | Gravity.CENTER);
         heading.setBackgroundColor(Color.YELLOW);
@@ -43,10 +45,20 @@ public class WorkingActivity extends Activity implements View.OnClickListener {
         Button btn1 = new Button(this);
         btn1.setWidth(10);
         btn1.setGravity(Gravity.CENTER);
+        btn1.setTextSize(TypedValue.COMPLEX_UNIT_SP, 25);
+        btn1.setTypeface(Typeface.SANS_SERIF, Typeface.BOLD);
         btn1.setText("Click Me With Any Style");
         layoutBody.addView(btn1);
 
-        TextView result = new TextView(this);
+        btn1.setOnClickListener(this);
+        btn1.setOnLongClickListener(new View.OnLongClickListener(){
+            public boolean onLongClick(View v) {
+                result.setText("You long pressed the button !!");
+                return true;
+            }
+        });
+
+        result = new TextView(this);
         result.setHeight(200);
         result.setGravity(Gravity.CENTER);
         result.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20);
@@ -59,6 +71,8 @@ public class WorkingActivity extends Activity implements View.OnClickListener {
 
     @Override
     public void onClick(View v) {
+
+        result.setText("You clicked the button !!");
 
     }
 }
